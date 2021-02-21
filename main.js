925df8d53ab2ac0370a7261bcdbd79509325c70c
soohyeok park <soohyeok.park@gmail.com>
2021-02-21 23:50:15 +0900

    folder add

--- main.js
+++ main.js
@@ -1,49 +1,53 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var as = require('async');
 const { Console } = require('console');
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathName = url.parse(_url, true).pathname;
-    var title = queryData.id;
+    var title = queryData.id;    
 
     if(title === undefined) {
         title = 'welcome';
     }
 
     if (pathName === '/') {
-        //console.log('>>>> ' + title);
         var fileName = title.toString().toLowerCase();
-        fs.readFile(`data/${fileName}`, 'utf8', function (err, data) {
-            var template = `
-        <!doctype html>
-        <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ol>
-                    <li><a href="?id=HTML">HTML</a></li>
-                    <li><a href="?id=CSS">CSS</a></li>
-                    <li><a href="?id=JavaScript">JavaScript</a></li>
-                </ol>
-                <h2>${title}</h2>
-                <!--<p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-                    <img src="coding.jpg" width="100%">
-                </p>
-                <p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-                </p>-->
-                ${data}
-            </body>
-        </html>
-        `;
-            response.writeHead(200);
-            response.end(template);
-        });
+        var dynamicList = new String("");
+        var dynamicContent = new String("");
+        var template = new String("asdf");
+
+        fs.readdir('./data', function(err, fileList) {
+            fileList.forEach(function(element) {
+                if(element != 'welcome') {
+                    dynamicList += `<li><a href="?id=${element}">${element}</a></li>`;
+                }                
+            });  
+            
+            fs.readFile(`./data/${fileName}`, 'utf8', function (err, content) {
+                dynamicContent = content;
+                template = `
+                <!doctype html>
+                <html>
+                    <head>
+                        <title>WEB1 - ${title}</title>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1><a href="/">WEB</a></h1>
+                        <ol>${dynamicList}</ol>
+                        <h2>${title}</h2>
+                        <p>${dynamicContent}</p>
+                    </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });    
+        });            
     } else {
         response.writeHead(404);
         response.end('Not found');
---
+++ trash/myfunc.js
@@ -0,0 +1,7 @@
+console.log(Math.round(1.6));
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(sum(1,14));
